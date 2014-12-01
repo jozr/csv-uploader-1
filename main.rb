8f0726a9f5e2fef0fadb8bfc59a26c272169456f
jozwright <jozwright@gmail.com>
2014-12-01 15:06:26 -0800

    saves file in uploads folder

--- main.rb
+++ main.rb
@@ -1,9 +1,20 @@
+require 'pg'
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'json'
 
 set :database, 'postgres://localhost/deal_a_day'
 
+class Detail < ActiveRecord::Base
+end
+
 get '/' do
   erb :index
+end
+
+post '/' do
+  File.open('uploads/' + params['upload'][:filename], "w") do |f|
+    f.write(params['upload'][:tempfile].read)
+  end
+  return "Your data was successfully uploaded!"
 end
