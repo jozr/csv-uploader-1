b1513b180bd709e2ab96aaf3007f9d433c5ba7cd
jozwright <jozwright@gmail.com>
2014-12-02 13:32:28 -0800

    remove column/change app structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201193952) do
+ActiveRecord::Schema.define(version: 20141202210836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20141201193952) do
   create_table "details", force: true do |t|
     t.string   "purchaser_name"
     t.string   "description"
-    t.decimal  "price", precision: 8, scale: 2
+    t.decimal  "price",          precision: 8, scale: 2
     t.integer  "amount"
     t.string   "address"
     t.string   "merchant_name"
     t.datetime "created_at"
-    t.datetime "updated_at"
   end
+
 end
--- main.rb
+++ main.rb
@@ -1,33 +1,11 @@
-require 'pg'
 require 'sinatra'
 require 'sinatra/flash'
 require 'sinatra/activerecord'
 
-set :database, 'postgres://localhost/deal_a_day'
-DB = PG.connect({:dbname => 'deal_a_day'})
-enable :sessions
+Dir.glob('./lib/*.rb').each { |f| require f }
 
-class Detail < ActiveRecord::Base
-  def self.upload(file)
-    File.open('uploads/' + file[:filename], "w") do |f|
-      f.write(file[:tempfile].read)
-    end
-    file_name = File.absolute_path('uploads/' + file[:filename]) 
-    DB.exec("COPY details(purchaser_name, description, price, amount, address, merchant_name) FROM '#{file_name}' WITH CSV HEADER DELIMITER AS ',';")
-    DB.exec("UPDATE details SET created_at = NOW() WHERE created_at IS NULL;")
-  end
-  
-  def self.latest_upload_revenue
-    total = 0
-    latest_upload = Detail.order('created_at').last
-    Detail.all.each do |d|
-      if d.created_at == latest_upload.created_at
-        total += d.price * d.amount
-      end
-    end
-    total
-  end
-end
+set :database, 'postgres://localhost/deal_a_day'
+enable :sessions
  
 get '/' do
   erb :index
