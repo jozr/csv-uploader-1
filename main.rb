7cf85619ce225621972a99cab844ba7db6a4971b
jozwright <jozwright@gmail.com>
2014-12-02 21:03:18 -0800

    link to homepage after sumbit

--- main.rb
+++ main.rb
@@ -12,10 +12,11 @@ get '/' do
 end
 
 post '/' do
+  link = "<a href=\"/\">Again?</a>"
   if params['upload']
     Detail.upload(params['upload'])
-    flash[:success] = "The total amount of revenue is $#{Detail.latest_upload_revenue}0. Lookin' good!"
+    flash[:success] = "The total amount of revenue is $#{Detail.latest_upload_revenue}0. Lookin' good!<br />#{link}"    
   else
-    flash[:error] = "You did not select a file to upload."
+    flash[:error] = "You did not select a file to upload.<br />#{link}"
   end
 end
