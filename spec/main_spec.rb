6ff2f35c935119a83f6e2c6a752970493b56a6cc
jozwright <jozwright@gmail.com>
2014-12-02 20:23:23 -0800

    test calculated revenue

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -8,5 +8,16 @@ describe Detail do
   it { should validate_presence_of :amount }
   it { should validate_presence_of :address }
   it { should validate_presence_of :merchant_name }
-  
+
+  it 'should calculate the total revenue from latest creation' do
+	@test = Detail.create(purchaser_name: 'David Bowie', 
+			  			  description: 'foam moon', 
+			  			  price: 100,
+			  			  amount: 2,
+			  			  address: '123 Main St.',
+			  			  merchant_name: 'The Labyrinth',
+			  			  created_at: Time.now)
+  	expect(Detail.latest_upload_revenue).to eql 200.0
+  	@test.delete
+  end
 end
