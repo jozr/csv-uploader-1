bf70252ddd2a055c3a55a047a6ce10e57342452f
jozwright <jozwright@gmail.com>
2014-12-02 22:57:18 -0800

    add user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202210836) do
+ActiveRecord::Schema.define(version: 20141203065510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20141202210836) do
     t.string   "address"
     t.string   "merchant_name"
     t.datetime "created_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
